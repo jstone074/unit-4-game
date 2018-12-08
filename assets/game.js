fb5ffd5c3eaeb56a3f34f60af319fa17d10b526f
Jimmy Stone <jstone05@hamline.edu>
2018-12-08 13:28:56 -0600

    updates

--- assets/game.js
+++ assets/game.js
@@ -1,25 +1,50 @@
-$(".card").on("click", function(){
+$(document).ready(function () {
 
-    $(this).addClass("bg-success");
+var characterSelection = false;
 
-    $(".card").not(this).addClass("bg-danger");
 
-    $("#select-Enemies").append($(".bg-danger"));
-    
 
-    
+$("#character-select .card").on("click", SelectCharacter
 
-    
  
-  
+    // $(this).addClass("bg-success");
+
+    // $(".card").not(this).addClass("bg-danger");
+    // $(".card").not(this).removeClass("preselect");
+
+    // $("#select-enemies").append($(".bg-danger"));
+    // $(".card").off("click",function(){});
+    // console.log(characterSelection);
+    
+)
+
+$(document).on("click", "#select-enemies .bg-danger", function() {
+
+})
+
+
+$("#select-enemies .bg-danger").on("click", function(){
+    console.log("DANGER ON CLICK");
+    $(this).removeClass(".bg-danger");
+    $(this).addClass(".bg-dark");
+    $("#select-defender").append($(this));
+
+})
+
+
+
+
 })
 
 
 function SelectCharacter () {
 
-    $(this).addClass("bg-primary");
+    $(this).addClass("bg-success");
 
+    $(".card").not(this).addClass("bg-danger");
+    $(".card").not(this).removeClass("preselect");
 
-    
+    $("#select-enemies").append($(".bg-danger"));
+    $(".card").off("click",SelectCharacter);
 
 }
